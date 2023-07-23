18985b47e8df72522f73b84c94c50e2dc5196dc7
shi3719 <shi3719@naver.com>
2023-07-24 05:07:21 +0900

    1차 시안 확인

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+//100vh 브라우저 환경 커버하기
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// resize
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+
+//남은시간 표시하기
+const time = document.querySelector('#time');
+
+const timeCalc = () => {
+    const endTime = new Date("2023-09-01");
+    const todayTime = new Date();
+    const leftTime = endTime - todayTime;
+
+    const leftDay = String(Math.floor(leftTime / (1000*60*60*24))).padStart(2, "0");
+    const leftHour = String(Math.floor((leftTime / (1000*60*60)) % 24)).padStart(2, "0");
+    const leftMin = String(Math.floor((leftTime / (1000*60)) % 60)).padStart(2, "0");
+    const leftSec = String(Math.floor(leftTime / 1000 % 60)).padStart(2, "0");
+    
+    time.innerHTML = `${leftDay}:${leftHour}:${leftMin}`
+}
+
+timeCalc();
+setInterval(timeCalc, 1000);
