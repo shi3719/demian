fe3eb887893797d66b952252e204bfcd01f1bed0
shi3719 <shi3719@naver.com>
2023-07-27 00:32:46 +0900

    링크 이동 수정

---
+++ assets/js/chapter.js
@@ -0,0 +1,100 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const chapterNum = urlParams.get('chapter');
+
+//enter 치환하기
+function changeText() {
+    const entertexts = document.querySelectorAll('.enter');
+
+    entertexts.forEach((entertext) => {
+        let textContent = entertext.innerHTML;
+
+        textContent = textContent.replace(/\r\n/ig, '<br>');
+        textContent = textContent.replace(/\\n/ig, '<br>');
+        textContent = textContent.replace(/\n/ig, '<br>');
+
+        entertext.innerHTML = textContent;
+    });
+}
+
+const articleWrap = document.querySelector('.article_wrap');
+fetchHTML(`/assets/includes/data${chapterNum}.html`)
+    .then((html) => {
+        articleWrap.innerHTML = html;
+        changeText();
+        goTop();
+    });
+
+const headingWrap = document.querySelector('.heading_wrap');
+fetchHTML(`/assets/includes/heading${chapterNum}.html`)
+    .then((html) => {
+        headingWrap.innerHTML = html;
+        subHeadingSet();
+    });
+
+
+//sub_heading btn 제어하기
+const getIndexOfElementInParent = (element) => {
+    const parent = element.parentNode;
+    if (!parent) {
+        return -1;
+    }
+
+    const children = parent.children;
+    let index = -1;
+
+    for (let i = 0; i < children.length; i++) {
+        if (children[i] === element) {
+            // 현재 요소를 찾으면 해당 인덱스를 반환합니다.
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+}
+
+const viewArticle = (num) => {
+    const articleWraper = document.querySelector('.article_wrap');
+
+    const articleWraps = Array.from(articleWraper.children);
+    articleWraps.forEach((articleWrap) => {
+        articleWrap.classList.remove('on');
+    })
+
+    articleWraps[num].classList.add('on');
+
+    window.scrollTo({
+        top: 0,
+        left: 0
+    })
+}
+
+const subHeadingSet = () => {
+    const subHeadingBtns = document.querySelectorAll('.sub_heading');
+    subHeadingBtns.forEach((subHeadingBtn) => {
+        subHeadingBtn.addEventListener('click', () => {
+            subHeadingBtns.forEach((subHeadingBtn) => {
+                subHeadingBtn.classList.remove('on');
+            })
+            subHeadingBtn.classList.add('on');
+            const num = getIndexOfElementInParent(subHeadingBtn);
+            viewArticle(num);
+        })
+    })
+}
+
+const goTop = () => {
+    //상단으로 바로가기 버튼
+    const topBtns = document.querySelectorAll('.top_btn');
+    topBtns.forEach((topBtn) => {
+        topBtn.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            })
+        })
+    })
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,3 @@
-w3.includeHTML();
-
 //100vh 브라우저 환경 커버하기
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -11,9 +9,9 @@ window.addEventListener('resize', () => {
 })
 
 //남은시간 표시하기
-const time = document.querySelector('#time');
-
 const timeCalc = () => {
+    const time = document.querySelector('#time');
+
     const endTime = new Date("2023-09-01");
     const todayTime = new Date();
     const leftTime = endTime - todayTime;
@@ -26,68 +24,20 @@ const timeCalc = () => {
     time.innerHTML = `${leftDay}:${leftHour}:${leftMin}`
 }
 
-timeCalc();
-setInterval(timeCalc, 1000);
-
-//sub_heading btn 제어하기
-
-const getIndexOfElementInParent = (element) => {
-    const parent = element.parentNode;
-    if (!parent) {
-        return -1;
-    }
-
-    const children = parent.children;
-    let index = -1;
-
-    for (let i = 0; i < children.length; i++) {
-        if (children[i] === element) {
-            // 현재 요소를 찾으면 해당 인덱스를 반환합니다.
-            index = i;
-            break;
-        }
-    }
-
-    return index;
+//html 넣어주기
+function fetchHTML(url) {
+    return fetch(url)
+        .then((response) => response.text())
+        .catch((error) => {
+            console.error('Error fetching HTML:', error);
+        });
 }
 
-const viewArticle = (num) => {
-    const articleWraper = document.querySelector('.article_wrap');
+const header = document.querySelector('header');
+fetchHTML('/assets/includes/header.html')
+    .then((html) => {
+        header.innerHTML = html;
 
-    const articleWraps = Array.from(articleWraper.children);
-    articleWraps.forEach((articleWrap) => {
-        articleWrap.classList.remove('on');
-    })
-
-    articleWraps[num].classList.add('on');
-
-    window.scrollTo({
-        top: 0,
-        left: 0
-    })
-}
-
-const subHeadingBtns = document.querySelectorAll('.sub_heading');
-subHeadingBtns.forEach((subHeadingBtn) => {
-    subHeadingBtn.addEventListener('click', () => {
-        subHeadingBtns.forEach((subHeadingBtn) => {
-            subHeadingBtn.classList.remove('on');
-        })
-        subHeadingBtn.classList.add('on');
-        const num = getIndexOfElementInParent(subHeadingBtn);
-        viewArticle(num);
-    })
-})
-
-
-//상단으로 바로가기 버튼
-const topBtns = document.querySelectorAll('.top_btn');
-topBtns.forEach((topBtn) => {
-    topBtn.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        })
-    })
-})
+        timeCalc();
+        setInterval(timeCalc, 1000);
+    });
